a7869ca9a1c13de5cc6c1bb67c37a5d77ea73e8b
MarceloVichar <marcos.marcelo43@gmail.com>
2022-09-29 15:32:24 -0300

    orientado a objeto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,105 @@
+class Bet {
+    constructor() {
+        this.id = 1
+        this.betArray = []
+    }
+    save() {
+        let bet = this.readData()
+        if (this.validate(bet)) {
+            document.getElementById('add').setAttribute('for', 'my-modal')
+            this.add(bet)
+        }
+        this.listBets()
+        this.clear()
+    }
+
+    add(bet) {
+        this.betArray.push(bet)
+        this.id++
+    }
+
+    readData() {
+        let bet = {}
+
+        bet.id = this.id
+        bet.event = document.getElementById('event').value
+        bet.value = document.getElementById('value').value
+        bet.ammount = document.getElementById('ammount').value
+
+        return bet
+    }
+
+    listBets(){
+        let tbody = document.querySelector('#tbody')
+        tbody.innerText = ''
+
+        for(let i = 0; i < this.betArray.length; i++) {
+            let tr = tbody.insertRow()
+
+            let td_id = tr.insertCell()
+            let td_event = tr.insertCell()
+            let td_value = tr.insertCell()
+            let td_ammount = tr.insertCell()
+            let td_actions = tr.insertCell()
+
+            td_id.innerText = this.betArray[i].id
+            td_event.innerText = this.betArray[i].event
+            td_value.innerText = `R$ ${this.betArray[i].value}`
+            td_ammount.innerText = `R$ ${this.betArray[i].ammount}`
+            let remove = document.createElement('span')
+            remove.innerText = 'X'
+            remove.style.color = 'red'
+            remove.setAttribute('onclick', 'bet.remove('+ this.betArray[i].id +')')
+            
+            td_actions.appendChild(remove)
+        }
+    }
+
+    validate(bet) {
+        let msg = ''
+        if (bet.event === '') {
+            msg += `Informe o evento \n`
+        }
+        if (bet.value === '') {
+            msg += `Informe o valor \n`
+        }
+        if (bet.ammount === '') {
+            msg += `Informe o retorno obtido \n`
+        }
+
+        if(msg !== '') {
+            alert(msg)
+            return false
+        }
+        return true
+    }
+
+    clear = () => {
+        document.getElementById('event').value = ''
+        document.getElementById('value').value = ''
+        document.getElementById('ammount').value = ''
+    }
+
+    remove = (id) => {
+        if (confirm('Deseja remover essa entrada?')) {
+            let tbody = document.querySelector('#tbody')
+            for(let i = 0; i < this.betArray.length; i++) {
+                if (this.betArray[i].id === id) {
+                    this.betArray.splice(i, 1)
+                    tbody.deleteRow(i)
+                }
+            }
+        }
+    }
+}
+
+let bet = new Bet
+
 let name = prompt("Qual seu nome?")
 document.getElementById('name').innerHTML = `Aqui estão seus lançamentos, ${name || ''}`
-alert(`Olá ${name}, seja bem vindo ao seu painel de apostas`)
 
 function onClickBalanceLink() {
-    confirm('Balanços estarão disponíveis em breve. Deseja ser avisado?')
+    alert('Balanços estarão disponíveis em breve.')
 }
 
 function adds() {
